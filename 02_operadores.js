b7111dec3147d734406d923323eff3d163dae41c
SmallPeterCo <elpedrobohorquez@outlook.com>
2025-12-09 20:55:39 -0500

    Add files via upload

---
+++ 02_operadores.js
@@ -0,0 +1,30 @@
+var number = num1 =5, num2=10;
+
+document.write("variable num1: " + num1 + " variable num2: " + num2 +"<br>");
+//operador suma
+var suma = num1 + num2;
+document.write("La suma es: " + suma + "<br>");
+
+//operador resta
+var resta = num2 - num1;
+document.write("La resta es: " + resta + "<br>");
+
+//operador multiplicacion
+var multiplicacion = num1 * num2;
+document.write("La multiplicacion es: " + multiplicacion + "<br>");
+
+//operador division
+var division = num2 / num1;
+document.write("La division es: " + division + "<br>");
+
+//operador modulo
+var modulo = num2 % num1;
+document.write("El modulo es: " + modulo + "<br>");
+
+//operador exponencial
+var exponencial = num1 ** num2;
+document.write("La exponencial es: " + exponencial + "<br>");
+
+//operador rauz cuadrada
+var raiz_cuadrada = Math.sqrt(num2);
+document.write("La raiz cuadrada es de num2 es: " + raiz_cuadrada + "<br>");
